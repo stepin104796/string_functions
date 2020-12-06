2a77c356c52c7ea1e258a7a7de8cc1f0a0f48156
Sindhu pitta <69101070+stepin104796@users.noreply.github.com>
2020-12-06 14:04:36 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -4,16 +4,27 @@
 
 int main()
 {
-    int a;
-    printf("choose the operation you want to perform:\n");
+     printf("choose the operation you want to perform:\n");
     printf("1.reading a string\n");
     printf("2.length of a string\n");
     printf("3.copy a string\n");
     printf("4.comparing two strings\n");
     printf("5.concat a string\n");
     printf("enter your choice:\n");
-    scanf("%d",&a);
-    select(a);
+
+    switch(select())
+    {
+        case 1 : readstring();
+        break;
+        case 2 : length();
+        break;
+        case 3 : copy();
+        break;
+        case 4 : compare();
+        break;
+        case 5 : concat();
+        break;
+    }
 
     return 0;
 }
