5141ede281f9e28db3899a88ea0ba6597d05cb8d
Sindhu pitta <69101070+stepin104796@users.noreply.github.com>
2020-12-06 13:25:30 +0530

    Add files via upload

---
+++ 3_Implementation/inc/string1.h
@@ -0,0 +1,20 @@
+#ifndef __STRING1_H__INCLUDED
+#define __STRING1_H__INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+#include <string.h>
+
+
+void readstring();
+
+void length();
+void copy();
+
+void compare();
+
+void concat();
+void select(int choice);
+
+#endif
